575410936b34f283b631b944a0f7679300b3f652
SYED JALEES AKBAR SHAH <161203405+syedjalees@users.noreply.github.com>
2024-03-13 18:43:05 +0500

    Add files via upload

---
+++ 1.ts
@@ -0,0 +1,25 @@
+// Question 1 = Node.js, Typescript and VsCode All are downloaded and installed in my laptop
+
+// Question 2 =  Personal Message: Store a person's name in a variable and pint a message to them, like "Hello Ashrib, would you like to learn some Typescript today?"
+// Code :
+let personName = "Asharib"
+console.log(`Hello ${personName}, Would you like to learn some Typescript today?`);
+
+// Output : Hello Asharib, Would you like to learn some Typescript today?
+
+// Question 3 = Name Cases: Store a person's name in a variable, and then print that person's name in lowercase,uppercaseand titlecase. 
+// Code : 
+let person_Name = "asharib"
+// lower case 
+console.log(person_Name.toLowerCase());
+// Uppercase 
+console.log(person_Name.toUpperCase());
+// Titlecase 
+console.log(person_Name.charAt(0).toUpperCase()+ person_Name.slice(1).toLowerCase());
+
+// Output :
+// asharib
+// ASHARIB
+// Asharib
+
+//                  Day 1 Coding Challenge is complete
